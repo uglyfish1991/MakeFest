838638b91ee63d91beb2feb8e3f614c3ae4e7d4b
Katy Long <katylong@michaelplewatch.home>
2022-06-07 14:43:39 +0100

    first push to github

--- search.py
+++ search.py
@@ -67,11 +67,35 @@ def search_a(time):
             search_a(time)
         else:
             return
+
     elif ans2=="3":
-        print(f"Searching for {ans1} in the User Database \n")
+        search_place="User Database"
+        print(f" {bcolors.OKGREEN} if {ans1} in {search_place} {bcolors.ENDC}")
+        print(f" {bcolors.OKGREEN}      print(all_occurences()) {bcolors.ENDC}\n \n")
         wait_text(f"{bcolors.OKBLUE}. . .{bcolors.ENDC}\n")
-        print(f"Found 1 instance of {ans1}")
+        fast_text(f"Searching for {ans1} in the {search_place} \n")
+        wait_text(f"{bcolors.OKBLUE}. . .{bcolors.ENDC}\n")
+        fast_text(f"{bcolors.OKCYAN}4 matching item(s) found in System Settings{bcolors.ENDC}\n")
+
+        print(f"""
+        {bcolors.WARNING}Name:{bcolors.ENDC} Batey, Stephen    {bcolors.WARNING}User Number:{bcolors.ENDC} LCC0374992     {bcolors.WARNING}Current:{bcolors.ENDC} 0 pending    {bcolors.WARNING}Notes:{bcolors.ENDC} N/A
+        {bcolors.WARNING}Name:{bcolors.ENDC} Davies, Kieran    {bcolors.WARNING}User Number:{bcolors.ENDC} LCC0600345     {bcolors.WARNING}Current:{bcolors.ENDC} 2 pending    {bcolors.WARNING}Notes:{bcolors.ENDC} N/A
+        {bcolors.WARNING}Name:{bcolors.ENDC} Gregor, James     {bcolors.WARNING}User Number:{bcolors.ENDC} LCC0998947     {bcolors.WARNING}Current:{bcolors.ENDC} Banned       {bcolors.WARNING}Notes:{bcolors.ENDC} {bcolors.FAIL}7 Unreturned - MAX FINE{bcolors.ENDC}
+        {bcolors.WARNING}Name:{bcolors.ENDC} Traynor, Mary     {bcolors.WARNING}User Number:{bcolors.ENDC} LCC0967783     {bcolors.WARNING}Current:{bcolors.ENDC} 7 pending    {bcolors.WARNING}Notes:{bcolors.ENDC} Pre-order BIThalo
+""")
+        fast_text("What would you like to do now? \n\n1 - Search Again? \n2 - Quit Search? \n Please type 1 or 2\n")
+        again=input("      >>    ")
+        while again !="1" and again !="2":
+            fast_text(f"{bcolors.WARNING}Unexpected input \n Please Try Again {bcolors.ENDC} \n Please type 1 or 2")
+            again=input("      >>    ")
+
+        if again=="1":
+            search_a(time)
+        else:
+            return
     elif ans2=="4":
         print(f"Searching for {ans1} in the Asset Catalogue \n")
         wait_text(f"{bcolors.OKBLUE}. . .{bcolors.ENDC}\n")
         print(f"Found 1 instance of {ans1}")
+
+search_a(4)
